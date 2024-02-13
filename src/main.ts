18316ff98be1ec0f626c8f663666a91d0951c1b6
CPimentel <cpimentel.0325@gmail.com>
2024-02-13 16:18:30 -0500

    add new domain

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ const port = process.env.PORT || 3000;
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://localhost:3000',
+    origin: 'https://pay-go.vercel.app',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     allowedHeaders: 'Content-Type, Accept, Authorization',
     credentials: true,
