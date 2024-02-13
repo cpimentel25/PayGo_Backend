bd75b440312112d8b4860e1261bb6ec8260eee20
CPimentel <cpimentel.0325@gmail.com>
2024-02-13 13:52:36 -0500

    update vercel config

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ExpressAdapter } from '@nestjs/platform-express';
-import * as express from 'express';
+import express from 'express';
 
 async function bootstrap() {
   const server = express();
   const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
-
   app.enableCors({
     origin: ['http://localhost:3000'],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
     allowedHeaders: 'Content-Type, Accept, Authorization',
   });
-
   await app.init();
-  return server;
+  return server; // Devuelve la instancia de Express
 }
 
-bootstrap();
+export default bootstrap();
