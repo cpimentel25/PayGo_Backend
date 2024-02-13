badd63a13759485f56368b6e1c3b5a02b2866b1c
CPimentel <cpimentel.0325@gmail.com>
2024-02-13 11:47:56 -0500

    update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
 import { ExpressAdapter } from '@nestjs/platform-express';
-import express from 'express';
-
-const server = express();
+import * as express from 'express';
 
 async function bootstrap() {
+  const server = express();
   const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
-  app.enableCors(); // Habilitamos CORS para pruebas locales
-  app.useGlobalPipes(new ValidationPipe()); // Validacion automatica
-  await app.listen(3001);
+  await app.init();
+  return server;
 }
 
-bootstrap();
+const serverPromise = bootstrap();
 
-export { server };
+export { serverPromise as server };
