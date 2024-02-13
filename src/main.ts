f80b5d8b892b628b4df3cbfe593d93d91f069df2
CPimentel <cpimentel.0325@gmail.com>
2024-02-13 11:39:12 -0500

    vercel config

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import express from 'express';
+
+const server = express();
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
   app.enableCors(); // Habilitamos CORS para pruebas locales
   app.useGlobalPipes(new ValidationPipe()); // Validacion automatica
   await app.listen(3001);
 }
 
 bootstrap();
+
+export { server };
